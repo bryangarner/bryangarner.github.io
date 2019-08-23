aeb909e266470507a6f11a66db1f9cc73045d0f7
bryangarner <37911216+bryangarner@users.noreply.github.com>
2019-08-23 18:36:02 -0400

    Update main.js

--- test/js/main.js
+++ test/js/main.js
@@ -1,11 +1,56 @@
-Webflow.push(function() {
-  $('.project-thumbnail').click(function(e) {
+$(document).ready(function() {
+  var $docEl = $('html, body'),
+    $wrap = $('.container'),
+    scrollTop;
+
+  $('button').click(function(e) {
+    overlayOpen();
     e.preventDefault();
-	$('body').css('overflow', 'hidden');
   });
 
   $('.close').click(function(e) {
+    overlayClose();
     e.preventDefault();
-	$('body').css('overflow', 'auto');
   });
+  
+  var overlayClose = function() {
+    $.unlockBody();
+    $('body').removeClass('overlay-open');
+  }
+  var overlayOpen = function() {
+    $('body').addClass('overlay-open');
+    $.lockBody();
+  }
+
+  $.lockBody = function() {
+    if(window.pageYOffset) {
+      scrollTop = window.pageYOffset;
+      
+      $wrap.css({
+        top: - (scrollTop)
+      });
+    }
+
+    $docEl.css({
+      height: "100%",
+      overflow: "hidden"
+    });
+  }
+
+  $.unlockBody = function() {
+    $docEl.css({
+      height: "",
+      overflow: ""
+    });
+
+    $wrap.css({
+      top: ''
+    });
+
+    window.scrollTo(0, scrollTop);
+    window.setTimeout(function () {
+      scrollTop = null;
+    }, 0);
+
+  }
 });
